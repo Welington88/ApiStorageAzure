2587c79beb6e3b7ffdd4a39c4437024b3d1516dc
Welington Marquezini Valverde da Silva <welington_marquezini88@live.com>
2023-01-09 19:29:58 -0300

    Salvar Arquivos e Log no Azure

---
+++ Controllers/ArquivosController.cs
@@ -0,0 +1,84 @@
+using ApiStorageAzure.Models;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiStorageAzure.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class ArquivosController : ControllerBase
+{
+    private readonly string _connectionString;
+    private readonly string _containerName;
+    #nullable disable
+    public ArquivosController(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetValue<string>("BlobConnectionString");
+        _containerName = configuration.GetValue<string>("BlobContainerName");
+    }
+
+    [HttpGet("Lista")]
+    public IActionResult ListaArquivo()
+    {
+
+        BlobContainerClient _containerClient = new(_connectionString, _containerName);
+
+        var blobsDto = new List<BlobDto>();
+        foreach (var blob in _containerClient.GetBlobs())
+        {
+            blobsDto.Add(new BlobDto {
+                Nome = blob.Name,
+                Tipo = blob.Properties.ContentType,
+                Uri  = _containerClient.Uri.AbsoluteUri + "/" + blob.Name
+            });
+        }
+
+        return Ok(blobsDto);
+    }
+
+    [HttpGet("Download/{nome}")]
+    public IActionResult DownloadArquivo(string nome)
+    {
+
+        BlobContainerClient _containerClient = new(_connectionString, _containerName);
+
+        BlobClient blob = _containerClient.GetBlobClient(nome);
+
+        if (!blob.Exists())
+        {
+            return BadRequest();
+        }
+        var retorno = blob.DownloadContent();
+        return File(retorno.Value.Content.ToArray(), retorno.Value.Details.ContentType, blob.Name);
+    }
+
+    [HttpPost("Upload/{arquivo}")]
+    public IActionResult UploadArquivo(IFormFile arquivo){
+
+        BlobContainerClient _containerClient  = new (_connectionString,_containerName);
+
+        BlobClient blob = _containerClient.GetBlobClient(arquivo.FileName);
+
+        using var data = arquivo.OpenReadStream();
+        blob.Upload(data, new BlobUploadOptions{
+            HttpHeaders = new BlobHttpHeaders{
+                ContentType = arquivo.ContentType
+            }
+        });
+
+        return Ok(blob.Uri.ToString());
+    }
+
+    [HttpDelete("Apagar/{nome}")]
+    public IActionResult DeleteArquivo(string nome)
+    {
+
+        BlobContainerClient _containerClient = new(_connectionString, _containerName);
+
+        BlobClient blob = _containerClient.GetBlobClient(nome);
+
+        blob.DeleteIfExists();
+        return NoContent();
+    }
+}
---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiStorageAzure.Models;
+using Azure.Data.Tables;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiStorageAzure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContatoController : ControllerBase
+    {
+        private readonly string _connectionString;
+        private readonly string _tableName;
+        #nullable disable
+        public ContatoController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetValue<string>("SacConnectionString");
+            _tableName = configuration.GetValue<string>("AzureTableName");
+        }
+
+        private TableClient GetTableClient() {
+            var serviceClient = new TableServiceClient(_connectionString);
+            var tableClient = serviceClient.GetTableClient(_tableName);
+
+            tableClient.CreateIfNotExists();
+            return tableClient;
+        }
+
+        [HttpGet("Lista")]
+        public IActionResult ListaContatos()
+        {
+            var tableClient = GetTableClient();
+            var contatos = tableClient.Query<Contatos>().ToList();
+
+            return Ok(contatos);
+        }
+
+        [HttpGet("ObterPorNome")]
+        public IActionResult ObterPorNome(string nome)
+        {
+            var tableClient = GetTableClient();
+            var contatos = tableClient.Query<Contatos>(c => c.Nome == nome).ToList();
+
+            return Ok(contatos);
+        }
+
+        [HttpPost]
+        public IActionResult Criar([FromBody] Contatos contato)
+        {
+            var tableClient = GetTableClient();
+
+            contato.RowKey = Guid.NewGuid().ToString();
+            contato.PartitionKey = contato.RowKey;
+
+            tableClient.UpsertEntity(contato);
+            return Ok(contato);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(string id, [FromBody] Contatos contato)
+        {
+            var tableClient = GetTableClient();
+            var contatoTable = tableClient.GetEntity<Contatos>(id,id).Value;
+
+            contatoTable.Nome = contato.Nome;
+            contatoTable.Email = contato.Email;
+            contatoTable.Telefone = contato.Telefone;
+
+            tableClient.UpsertEntity(contatoTable);
+            return Ok(contatoTable);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var tableClient = GetTableClient();
+            var contatoTable = tableClient.DeleteEntity(id, id);
+
+            return NoContent();
+        }
+    }
+}
